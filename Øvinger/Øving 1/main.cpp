b49429a8edd9d4e5e0cbc5ddd2021547f957fca3
JakobFE04 <eikeland.jakob@gmail.com>
2025-01-12 13:48:41 +0100

    Added Øving 0 and 1

---
+++ Øvinger/Øving 1/main.cpp
@@ -0,0 +1,54 @@
+// Øving 1
+// Oversettelse fra Python til C++
+#include "std_lib_facilities.h"
+
+int maxOfTwo(int a, int b)   {
+    // BEGIN: 2a
+	if (a > b){
+        cout << "A is greater than "
+    }
+    // END: 2a
+}
+
+int fibonacci(int n) {
+    // BEGIN: 2c
+
+    // END: 2c
+}
+
+int squareNumberSum(int number) {
+    // BEGIN: 2d
+
+    // END: 2d
+}
+
+void triangleNumbersBelow(int number) {
+    // BEGIN: 2e
+
+    // END: 2e
+}
+
+bool isPrime(int number) {
+    // BEGIN: 2f
+
+    // END: 2f
+}
+
+void naivePrimeNumberSearch(int maxNumber) {
+    // BEGIN: 2g
+
+    // END: 2g 
+}
+
+void inputAndPrintNameAndAge() {
+    // BEGIN: 2h
+
+    // END: 2h
+}
+
+int main() {
+    // BEGIN: 2b
+
+    // END: 2b
+	return 0;
+}
